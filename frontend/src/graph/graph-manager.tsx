a8b0893cd25de92fc2bb47565c8b867dae50c971
sarah <sarah.vogels@aol.de>
2025-05-09 16:09:50 +0200

    basic react flow setup

---
+++ frontend/src/graph/graph-manager.tsx
@@ -0,0 +1,49 @@
+import { ReactFlow, Controls, type NodeOrigin, Panel } from "@xyflow/react";
+
+// we have to import the React Flow styles for it to work
+import "@xyflow/react/dist/style.css";
+import useStore, { type RFState } from "./store.ts";
+import { shallow } from "zustand/vanilla/shallow";
+import { CustomNode } from "../components";
+
+const selector = (state: RFState) => ({
+  nodes: state.nodes,
+  edges: state.edges,
+  onNodesChange: state.onNodesChange,
+  onEdgesChange: state.onEdgesChange,
+});
+
+// this places the node origin in the center of a node
+const nodeOrigin: NodeOrigin = [0.5, 0.5];
+
+//custom node
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+function Flow() {
+  // whenever you use multiple values, you should use shallow to make sure the component only re-renders when one of the values changes
+  const { nodes, edges, onNodesChange, onEdgesChange } = useStore(
+    selector,
+    shallow,
+  );
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      nodeTypes={nodeTypes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      nodeOrigin={nodeOrigin}
+      fitView
+    >
+      <Controls showInteractive={false} />
+      <Panel position="top-left">
+        Proteoform graph visualization with React Flow library
+      </Panel>
+    </ReactFlow>
+  );
+}
+
+export default Flow;
