6cdf16959f89f1a69a32f31ba76eb2c73f5fe204
sarah <sarah.vogels@aol.de>
2026-01-31 00:23:36 +0200

    feat(search-visual): added hightlights to minimap and sequenceNode
    also added colors to theme

--- frontend/src/components/sequence-node/sequence-container/sequence-container.tsx
+++ frontend/src/components/sequence-node/sequence-container/sequence-container.tsx
@@ -1,20 +1,22 @@
 import styled from "styled-components";
 import { nodeWidthModes } from "../../../theme/types.tsx";
+import { theme } from "../../../theme";
 
 interface SequenceContainerProps {
   nodeWidthMode?: nodeWidthModes;
   sequence: string;
+  isReversed?: boolean;
   containerWidthRef: React.RefObject<HTMLDivElement> | null;
+  searchResultIndices?: {
+    startIndex: number;
+    endIndex: number;
+  };
 }
 
-const CollapsedSequence = styled.div`
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 18px;
-  white-space: normal;
+const CollapsedContainer = styled.div`
   max-width: 100px;
   overflow: auto;
   cursor: pointer;
-  // make text selectable and copyiable
   user-select: text;
 
   &::-webkit-scrollbar {
@@ -29,27 +31,63 @@ const CollapsedSequence = styled.div`
 `;
 
 const Sequence = styled.div`
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-family: "Inter", sans-serif;
   font-size: 18px;
   color: #000;
   white-space: normal;
-  overflow: hidden;
   cursor: pointer;
-  // make text selectable and copyiable
   user-select: text;
 `;
 
 export const SequenceContainer: React.FC<SequenceContainerProps> = ({
   nodeWidthMode,
   sequence,
+  isReversed = false,
   containerWidthRef,
+  searchResultIndices,
 }) => {
+  const { startIndex, endIndex } = searchResultIndices || {};
+
+  const renderSequence = () => {
+    if (startIndex !== undefined && endIndex !== undefined) {
+      const length = sequence.length;
+      const before = isReversed
+        ? sequence.slice(0, length - endIndex)
+        : sequence.slice(0, startIndex);
+      const highlighted = isReversed
+        ? sequence.slice(length - endIndex, length - startIndex)
+        : sequence.slice(startIndex, endIndex);
+      const after = isReversed
+        ? sequence.slice(length - startIndex, length)
+        : sequence.slice(endIndex);
+
+      return (
+        <>
+          <span style={{ color: "black" }}>{before}</span>
+          <span
+            style={{
+              color: theme.searchResult.matchingAminoAcid,
+              fontWeight: "bold",
+              // backgroundColor: "red",
+            }}
+          >
+            {highlighted}
+          </span>
+          <span style={{ color: "black" }}>{after}</span>
+        </>
+      );
+    }
+    return <span style={{ color: "black" }}>{sequence}</span>;
+  };
+
   return (
     <div ref={containerWidthRef}>
       {nodeWidthMode === nodeWidthModes.Expanded ? (
-        <Sequence>{sequence}</Sequence>
+        <Sequence>{renderSequence()}</Sequence>
       ) : nodeWidthMode === nodeWidthModes.Small ? (
-        <CollapsedSequence>{sequence}</CollapsedSequence>
+        <CollapsedContainer>
+          <Sequence>{renderSequence()}</Sequence>
+        </CollapsedContainer>
       ) : (
         <div></div>
       )}
